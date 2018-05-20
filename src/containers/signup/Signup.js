fef17d8137ec85c5eabdeb963cc32fb3a0907230
David <davjue@gmail.com>
2018-05-20 02:14:16 -0700

    added constraints to prevent empty username or passwords and pass confirmations

--- src/containers/signup/Signup.js
+++ src/containers/signup/Signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Route, Redirect} from 'react-router-dom'
 
 class Signup extends Component {
   constructor(props) {
@@ -8,7 +9,8 @@ class Signup extends Component {
       username: '',
       email: '',
       password: '',
-      password_confirmation: ''
+      password_confirmation: '',
+      error_message: ''
     }
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this)
@@ -21,8 +23,48 @@ class Signup extends Component {
   }
 
   onSubmit(e) {
-    e.preventDefault()
-    axios.post('http://localhost:8000/api/users', {user: this.state})
+    console.log(this.state);
+    e.preventDefault();
+    if (this.state.username.length !== 0 && this.state.password.length !== 0 && this.state.password_confirmation.length !== 0
+      && this.state.password === this.state.password_confirmation) {
+
+      axios.post('http://localhost:8000/api/users', {user: this.state})
+      .then((response) => {
+        this.setState({
+          error_message: ''
+        });
+        this.props.history.push('/login');
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          error_message: 'Username already exists.'
+        })
+      })
+    }
+    else if (this.state.username.length === 0) {
+      this.setState({
+        error_message: 'Please fill in username.'
+      })
+    }
+
+    else if (this.state.password.length === 0) {
+      this.setState({
+        error_message: 'Please enter a password.'
+      })
+    }
+
+    else if (this.state.password_confirmation.length === 0) {
+      this.setState({
+        error_message: 'Please confirm your password in the password reconfirmation box.'
+      })
+    }
+
+    else if (this.state.password !== this.state.password_confirmation) {
+      this.setState({
+        error_message: 'Your password and password confirmation do not match.'
+      })
+    }
   }
 
   render() {
@@ -30,6 +72,7 @@ class Signup extends Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
+            <h1>{this.state.error_message}</h1>
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <label className="control-label">Username</label>
