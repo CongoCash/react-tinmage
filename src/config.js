171a858a9a23d2641ae5f341a753c41e46df3057
David <davjue@gmail.com>
2018-08-06 21:37:08 -0700

    deploying to heroku

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const myConfig = { api_url: 'https://stark-forest-85024.herokuapp.com/'};
-export const myConfig = { api_url: 'http://localhost:8000/'};
+export const myConfig = { api_url: 'https://stark-forest-85024.herokuapp.com/'};
+// export const myConfig = { api_url: 'http://localhost:8000/'};
